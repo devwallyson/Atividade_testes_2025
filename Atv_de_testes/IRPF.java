38d6599a6e7af0f97e792669f071423617dcf907
Wallyson Souza <120031974+devwallyson@users.noreply.github.com>
2025-06-12 22:23:32 -0300

    Add files via upload

---
+++ Atv_de_testes/IRPF.java
@@ -0,0 +1,46 @@
+package app;
+
+public class IRPF {
+
+    private float salario;
+    private float contribuicaoPrevidenciaria;
+    private String[] dependentes;
+
+    public IRPF() {
+        dependentes = new String[0];
+    }
+
+    public void cadastrarSalario(float salario) {
+        this.salario += salario;
+    }
+
+    public float getTotalSalario() {
+        return this.salario;
+    }
+
+    public void cadastrarPrevidenciaOficial(float contribuicaoPrevidenciaria) {
+        this.contribuicaoPrevidenciaria = contribuicaoPrevidenciaria;
+    }
+
+    public float getPrevidenciaOficial() {
+        return this.contribuicaoPrevidenciaria;
+    }
+
+    public void cadastrarDependente(String nome) {
+        
+        String[] temp = new String[dependentes.length + 1];
+        for (int i = 0; i < dependentes.length; i++) {
+            temp[i] = dependentes[i];
+        }
+
+        
+        temp[dependentes.length] = nome;
+
+        
+        dependentes = temp;
+    }
+
+    public float getDeducaoDependentes() {
+        return dependentes.length * 189.59f;
+    }
+}
